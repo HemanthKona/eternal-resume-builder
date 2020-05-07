f1d257223e5229f1ff56f082d704aa982a93fad2
Hemanth Kona <hemanth0690@gmail.com>
2020-05-06 23:50:11 -0400

    Test screenshot

--- apps/functions/generate-pdf/generate-pdf.js
+++ apps/functions/generate-pdf/generate-pdf.js
@@ -1,6 +1,36 @@
-exports.handler = (event, context, callback) => {
-  callback(null, {
-    statusCode: 400,
-    body: "Test Error"
+const chromium = require('chrome-aws-lambda');
+
+exports.handler = async (event, context) => {
+
+  // const pageToScreenshot = JSON.parse(event.body).pageToScreenshot;
+  const pageToScreenshot = 'https://eternallife.live';
+
+  // if (!pageToScreenshot) return {
+  //   statusCode: 400,
+  //   body: JSON.stringify({ message: 'Page URL not defined' })
+  // }
+
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
   });
+
+  const page = await browser.newPage();
+
+  await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
+
+  const screenshot = await page.screenshot({ encoding: 'binary' });
+
+  await browser.close();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Complete screenshot of ${pageToScreenshot}`,
+      buffer: screenshot
+    })
+  }
+
 }
