189097c708bc5dd814d6616e075b1262c4147714
Hemanth Kona <hemanth0690@gmail.com>
2020-05-08 22:25:50 -0400

    Try pdf

--- apps/functions/generate-pdf/generate-pdf.js
+++ apps/functions/generate-pdf/generate-pdf.js
@@ -21,16 +21,22 @@ exports.handler = async (event, context) => {
 
   await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
 
-  const screenshot = await page.screenshot({ encoding: 'binary' });
+  // const screenshot = await page.screenshot({ encoding: 'binary' });
+  const pdf = await page.pdf();
 
   await browser.close();
 
   return {
+    headers: {
+      'Content-type': 'application/pdf',
+      'content-disposition': 'attachment; filename=test.pdf'
+    },
     statusCode: 200,
     body: JSON.stringify({
       test: 'test working',
       message: `Complete screenshot of ${pageToScreenshot}`,
-      buffer: screenshot
+      // buffer: screenshot,
+      pdf: pdf.toString('base64')
     })
   }
 
