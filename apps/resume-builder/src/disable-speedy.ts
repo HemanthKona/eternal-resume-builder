398bcf975705d3bbf04e63141a4edd3e19099a7b
Hemanth Kona <hemanth0690@gmail.com>
2020-05-12 23:04:12 -0400

    emotion: Disable speedy

---
+++ apps/resume-builder/src/disable-speedy.ts
@@ -0,0 +1,8 @@
+import { sheet } from 'emotion'
+
+// Check if the root node has any children to detect if the app has been preprendered
+// speedy is disabled when the app is being prerendered so that styles render into the DOM
+// speedy is significantly faster though so it should only be disabled during prerendering
+if (!document.getElementById('root').hasChildNodes()) {
+  sheet.isSpeedy = false;
+}
--- apps/resume-builder/src/main.tsx
+++ apps/resume-builder/src/main.tsx
@@ -1,3 +1,4 @@
+import './disable-speedy'
 import React from 'react';
 import ReactDOM from 'react-dom';
 
