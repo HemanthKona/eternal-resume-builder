ce986e7ee58f959ad3bc20f1587f13f77a552e38
Hemanth Kona <hemanth0690@gmail.com>
2021-01-27 19:08:26 -0500

    Remove default data

--- apps/next-resume-builder/pages/_app.tsx
+++ apps/next-resume-builder/pages/_app.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
+
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to next-resume-builder!</title>
+        <title>Resumer.dev!</title>
       </Head>
       <div className="app">
         <header className="flex">
-          <NxLogo width="75" height="50" />
-          <h1>Welcome to next-resume-builder!</h1>
+          <h1>Welcome to Resumer.dev!</h1>
         </header>
         <main>
           <Component {...pageProps} />
